3059b7a01eb7aa771e3615764a234751e2f842bf
Tonyligogo <tonyligogo@gmail.com>
2023-05-25 16:54:10 +0300

    2nd trial

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.This is a trial.
+          Edit <code>src/App.js</code> and save to reload.This is a trial.This is second trial.
         </p>
         p
         <a
